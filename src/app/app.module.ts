0195d753d1365d32993937d367c31922c60d48f4
Stephen Gachoka <stephengachoka57@gmail.com>
2022-02-17 15:42:04 +0300

    pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,9 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NotificationPipe } from './pages/notification.pipe';
-import { HomePipe } from './pages/home.pipe';
-import { RecordsPipe } from './pages/records.pipe';
 
 @NgModule({
-  declarations: [AppComponent, NotificationPipe, HomePipe, RecordsPipe],
+  declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
