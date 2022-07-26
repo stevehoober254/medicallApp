4925c757923debe0d938652676a237a99b818e5d
Stephen Gachoka <stephengachoka57@gmail.com>
2022-07-26 17:12:38 +0300

    tp

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  @ViewChild('slides', { static: true }) slides: IonSlides;
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400,
+    slidesPerView: 2.1,
+  };
+  sliderItems = [
+    { active: true, name: 'Michael Simpsom' },
+    { active: false, name: 'Kate Brown' },
+    { active: false, name: 'Jack Sparrow' },
+    { active: false, name: 'Elsie Roy' },
+  ];
+  patientProfiles = [
+    {image:'assets/images/prof1.png'},
+    {image:'assets/images/prof2.png'},
+    {image:'assets/images/prof3.png'},
+    {image:'assets/images/prof3.png'}
+  ];
+  activeSlideIndex = 0;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  slideChanged() {
+    this.slides.getActiveIndex().then((index: number) => {
+      console.log(index);
+      this.activeSlideIndex = index;
+    });
   }
-
 }
